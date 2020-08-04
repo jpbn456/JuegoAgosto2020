f20cba43f0eb9d0f4293fd1fa29b0be80eaa0606
FabriLM98 <63129784+FabriLM98@users.noreply.github.com>
2020-08-04 20:48:48 -0300

    Creada Game Scene

---
+++ Brackeys-Jam/Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static bool MouseClicked = false;
+    public static bool KeyPressed = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MouseClicked = true;
+        }
+        else
+        {
+            MouseClicked = false;
+        }
+        if(!MouseClicked && Input.anyKeyDown)
+        {
+            KeyPressed = true;
+        }
+        if (!Input.anyKeyDown)
+        {
+            KeyPressed = false;
+        }
+    }
+}
