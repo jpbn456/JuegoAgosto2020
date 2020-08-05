b75594cdc652a23d1ba3cd58be764a6c62286054
FabriLM98 <63129784+FabriLM98@users.noreply.github.com>
2020-08-04 22:27:19 -0300

    Cambio de pestanias agregado

---
+++ Brackeys-Jam/Assets/Scripts/Cuenta.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Cuenta
+{
+    public string nombreUsuario;
+    public string contrasenia;
+    //0 nomral, -1 dislike, 1 like
+    public int estadoBoton = 0;
+}
---
+++ Brackeys-Jam/Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject[] pestanias;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CambiarPestania(int pestaniaNueva)
+    {
+        for (int i = 0; i < pestanias.Length; i++)
+        {
+            if( i != pestaniaNueva)
+            {
+                pestanias[i].SetActive(false);
+            }
+            else
+            {
+                pestanias[i].SetActive(true);
+            }
+        }
+    }
+}
